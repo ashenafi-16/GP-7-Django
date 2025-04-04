2aeb5558f93ce70c03c0e754a3844783737299ee
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-04-04 08:05:12 -1100

    Added  logout and login features

--- Social_Media_API/users/views.py
+++ Social_Media_API/users/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth import logout # type: ignore
-def user_logout(request):
-    logout(request)
-    return redirect('login')
+from rest_framework.views import APIView # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework.permissions import IsAuthenticated # type: ignore
+from rest_framework_simplejwt.tokens import RefreshToken # type: ignore
 
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh_token")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message": "Successfully logged out"}, status=200)
+        except Exception as e:
+            return Response({"error": "Invalid token"}, status=400)
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+
+        if not user.check_password(old_password):
+            return Response({"error": "Old password is incorrect"}, status=400)
+
+        user.set_password(new_password)
+        user.save()
+
+        return Response({"message": "Password changed successfully. Please log in again."}, status=200)
