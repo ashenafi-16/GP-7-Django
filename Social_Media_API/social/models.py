b2e311c3e78dcfe72bb228b36db9c50a37f32078
Amanuel Wubishet <biganuel6792@gmail.com>
2025-04-04 23:13:47 +0300

    add like and follow model

--- Social_Media_API/social/models.py
+++ Social_Media_API/social/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+
 User = get_user_model()
 
 class Post(models.Model):
@@ -31,4 +29,25 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user.username}: {self.content[:50]}..."
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = {'user', 'post'}
     
+    def __str__(self):
+        return f"{self.user.username} liked Post {self.post.id}"
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    followed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')  # prevent duplicate follows
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
