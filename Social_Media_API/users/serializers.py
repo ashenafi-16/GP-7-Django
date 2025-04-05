33f9b0acfcae59eac7fef7ff64ff441f18a6fe48
Redeat-Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2025-04-04 18:46:51 -1100

    implement logout and password change using serializers

--- Social_Media_API/users/serializers.py
+++ Social_Media_API/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User 
+from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -23,3 +24,15 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+class LogoutSerializer(serializers.Serializer):
+    refresh_token = serializers.CharField()
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        validate_password(value) 
+        return value
