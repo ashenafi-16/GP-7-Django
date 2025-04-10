dd3c08897d6f0ec8c01e4cdb8cf0db2a15a0e671
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2025-04-10 15:25:02 +0300

    capstone GDG project

--- Social_Media_API/social/views.py
+++ Social_Media_API/social/views.py
@@ -2,6 +2,7 @@ from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 from .models import Post, Comment, Like, Follow
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
